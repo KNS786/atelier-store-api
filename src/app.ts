9564c6d13c29c88ecceb45cd5697ad672d101690
kns786 <doolocompany@gmail.com>
2026-01-09 06:16:15 +0530

    feat: added cors origin error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import  authRoutes from './routes/auth.routes';
 
-
-
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
